8ea55d53f6feab5c7a0d980ecd61bfabbfae35d1
ballihakkari <ballibronze@gmail.com>
2018-10-13 16:55:38 +0000

    if one number is enserted, add returns that number

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,4 +1,7 @@
 function Add(numbers){
-    return 0;
+    if(numbers == ""){
+        return 0;
+    }
+    return parseInt(numbers);
 }
-module.exports = Add;
+ module.exports = Add;
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,6 @@ const Add = require('./StringCalculator');
 test("An empty string will return 0", () => {
   expect(Add("")).toBe(0);
 });
+test("If one number is inserted, return that number", () => {
+  expect(Add("5")).toBe(5);
+});
